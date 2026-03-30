0d3da395e9b7c31e610c658cf319fb3a76b2eae3
frlc <frlc_31@hotmail.com>
2026-03-29 21:22:17 -0300

    Adequando teste a padrão AAA

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,15 +1,19 @@
 import { test, expect } from '@playwright/test'
 
 test('deve consulta um pedido aprovado', async ({ page }) => {
+//Arrange
   await page.goto('http://localhost:5173/')
-
   // Checkpoint 1: Verificar se a página está online
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
   await page.getByRole('link', { name: 'Consultar Pedido' }).click()
   // Checkpoint 2: Verificar se a página de consulta de pedido está carregada
-  await expect(page.getByRole('heading')).toContainText('Consultar Pedido')  
+  await expect(page.getByRole('heading')).toContainText('Consultar Pedido') 
+
+//Act
   await page.getByTestId('search-order-id').fill('VLO-4V79FY')
   await page.getByTestId('search-order-button').click()
+
+//Assert
   // Checkpoint 3: Verificar se o pedido foi encontrado
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-4V79FY')
   await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
