e3e315c550efb9e66a64564a97ea7de5234c53b8
frlc <frlc_31@hotmail.com>
2026-04-04 17:03:12 -0300

    Ajuste criação de hooks

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,60 +1,65 @@
 import { test, expect } from '@playwright/test'
 import { generateOrderCode } from '../support/helpers'
 
-test('deve consulta um pedido aprovado',{ tag: '@TC-002' }, async ({ page }) => {
+test.describe('Consulta de pedidos', () => {
 
-  //Test data:
-  const orderId = 'VLO-4V79FY'
-  const orderStatus = 'APROVADO'
+ 
+  test.beforeEach(async ({page}) => {
 
-  //Arrange
-  await page.goto('http://localhost:5173/')
-  // Checkpoint 1: Verificar se a página está online
-  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
-  await page.getByRole('link', { name: 'Consultar Pedido' }).click()
-  // Checkpoint 2: Verificar se a página de consulta de pedido está carregada
-  await expect(page.getByRole('heading')).toContainText('Consultar Pedido') 
-
-  //Act
-  await page.getByTestId('search-order-id').fill(orderId)
-  await page.getByTestId('search-order-button').click()
-
-  //Assert
-  // Checkpoint 3: Verificar se o pedido foi encontrado
-  await expect(page.getByTestId('order-result-VLO-4V79FY')).toBeVisible({timeout: 15_000})  
-
-  //code normal
-  await expect(page.getByTestId('order-result-id')).toContainText(orderId)
-  await expect(page.getByTestId('order-result-status')).toContainText(orderStatus)
-
-  //Challenge solution 1:
-  // await expect(page.locator('p.font-mono.font-medium')).toContainText('VLO-4V79FY')
-  // await expect(page.locator('div.bg-green-100.text-green-700')).toContainText('APROVADO')
-  
-  //Challenge solution 2:
-  // await expect(page.getByTestId('order-result-VLO-4V79FY')).toContainText('VLO-4V79FY')
-  // await expect(page.getByTestId('order-result-VLO-4V79FY')).toContainText('APROVADO')
-
-  //Challenge class:
-  // const containerPedido = page.getByRole('paragraph')
-  //.filter({ hasText: '/^Pedido$/' })//Expressão regular para pegar o texto 'Pedido'
-  // .locator('..') //Sobe um nivel e pega o parent do p que tem o texto 'Pedido'
-  //await expect(containerPedido).toContainText('VLO-4V79FY')
-
-})
-
-test('deve exibir mensagem de pedido nao encontrado',{ tag: '@TC-003' }, async ({ page }) => {
-
-    //Test data:
-    const orderId = generateOrderCode()
-  
-    //Arrange
     await page.goto('http://localhost:5173/')
     // Checkpoint 1: Verificar se a página está online
     await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
     await page.getByRole('link', { name: 'Consultar Pedido' }).click()
     // Checkpoint 2: Verificar se a página de consulta de pedido está carregada
     await expect(page.getByRole('heading')).toContainText('Consultar Pedido') 
+    
+  })
+ 
+
+  test('deve consulta um pedido aprovado',{ tag: '@TC-002' }, async ({ page }) => {
+
+    //Test data:
+    const orderId = 'VLO-4V79FY'
+    const orderStatus = 'APROVADO'
+  
+    //Arrange
+    
+  
+    //Act
+    await page.getByTestId('search-order-id').fill(orderId)
+    await page.getByTestId('search-order-button').click()
+  
+    //Assert
+    // Checkpoint 3: Verificar se o pedido foi encontrado
+    await expect(page.getByTestId('order-result-VLO-4V79FY')).toBeVisible({timeout: 15_000})  
+  
+    //code normal
+    await expect(page.getByTestId('order-result-id')).toContainText(orderId)
+    await expect(page.getByTestId('order-result-status')).toContainText(orderStatus)
+  
+    //Challenge solution 1:
+    // await expect(page.locator('p.font-mono.font-medium')).toContainText('VLO-4V79FY')
+    // await expect(page.locator('div.bg-green-100.text-green-700')).toContainText('APROVADO')
+    
+    //Challenge solution 2:
+    // await expect(page.getByTestId('order-result-VLO-4V79FY')).toContainText('VLO-4V79FY')
+    // await expect(page.getByTestId('order-result-VLO-4V79FY')).toContainText('APROVADO')
+  
+    //Challenge class:
+    // const containerPedido = page.getByRole('paragraph')
+    //.filter({ hasText: '/^Pedido$/' })//Expressão regular para pegar o texto 'Pedido'
+    // .locator('..') //Sobe um nivel e pega o parent do p que tem o texto 'Pedido'
+    //await expect(containerPedido).toContainText('VLO-4V79FY')
+  
+  })
+
+  test('deve exibir mensagem de pedido nao encontrado',{ tag: '@TC-003' }, async ({ page }) => {
+
+    //Test data:
+    const orderId = generateOrderCode()
+  
+    //Arrange
+    
   
     //Act
     await page.getByTestId('search-order-id').fill(orderId)
@@ -75,3 +80,9 @@ test('deve exibir mensagem de pedido nao encontrado',{ tag: '@TC-003' }, async (
     `)
       
   })
+  
+})
+
+
+
+
